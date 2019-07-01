f2f19bba5397372fe1d86ffc1ce97e5b14084697
Robert Gersak <robi@neyho.com>
2019-07-01 15:21:15 +0200

    Filtering Paths

--- routes/admin.js
+++ routes/admin.js
@@ -7,14 +7,14 @@ router.get('/add-product', (req, res, next) => {
   console.log('In another Middleware - Products page!')
   res.send(`
     <h1>Add Product page!</h1>
-    <form action="/product" method="post">
+    <form action="/admin/add-product" method="post">
       <input type="text" name="title">
       <button type="submit">Submit</button>
     </form>
   `)
 })
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body)
   res.redirect('/')
 })
