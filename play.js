45f1fd4bd125db4891a82ede766c62767c156840
Robert Gersak <robi@neyho.com>
2019-07-01 07:01:32 +0200

    Arrays & Array Method

--- play.js
+++ play.js
@@ -1,16 +1,18 @@
 const person = {
   name: 'Klop',
   age: 35,
-  hasHobbies: true,
-  // greet: () => {
-  //   console.log('Hi, I am ' + this.name)
-  // }
   greet() {
     console.log('Hi, I am ', this.name)
   }
 }
 
-// This returns "undefined"
-// console.log(person.greet())
+const hobbies = ['Sports', 'Cooking']
+// for (let hobby of hobbies) {
+//   console.log(hobby)
+// }
 
-person.greet()
+// console.log('NEW: ', hobbies.map(hobby => {
+//   return 'Hobby: ' + hobby
+// }))
+console.log('NEW: ', hobbies.map(hobby => 'Hobby: ' + hobby))
+console.log('OLD: ', hobbies)
