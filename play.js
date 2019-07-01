5d8c2aaefa944ae63768a4057e9fff12918095dd
Robert Gersak <robi@neyho.com>
2019-07-01 07:43:25 +0200

    Async Code & Promises

--- play.js
+++ play.js
@@ -1,69 +1,24 @@
-const person = {
-  name: 'Klop',
-  age: 35,
-  greet() {
-    console.log('Hi, I am ', this.name)
-  }
-}
-// DESTRUCTURING
-// const printName = (personData) => {
-//   console.log(personData.name)
-// }
-
-const printName = ({ name, age }) => {
-  console.log(name)
-  console.log(age)
+const fetchData = () => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!')
+    }, 1500)
+  })
+  return promise
 }
 
-printName(person)
+// This is async code because it does not execute immediately
+setTimeout(() => {
+  console.log('Timer is done')
+  fetchData()
+  .then(text => {
+    console.log(text)
+    return fetchData()
+  })
+  .then(text2 => {
+    console.log(text2)
+  })
+}, 2000)
 
-
-const { name, age } = person
-console.log(name, age)
-
-// const copied_person = {...person}
-// console.log(copied_person)
-//
-const hobbies = ['Sports', 'Cooking']
-const [hobby1, hobby2] = hobbies
-console.log(hobby1)
-console.log(hobby2)
-
-const [test_hobby1, test_hobby2] = hobbies
-console.log(test_hobby1)
-console.log(test_hobby2)
-
-
-// for (let hobby of hobbies) {
-//   console.log(hobby)
-// }
-
-// console.log('NEW: ', hobbies.map(hobby => {
-//   return 'Hobby: ' + hobby
-// }))
-// console.log('NEW: ', hobbies.map(hobby => 'Hobby: ' + hobby))
-// console.log('OLD: ', hobbies)
-// hobbies.push('Programming')
-// console.log(hobbies)
-
-// REST and SPREAD operators
-// If we want to add element to an array, and we don't want to edit the original array,
-// but create a new array with all the old values and add to that copied array.
-// This is common patern called IMMUTABILITY - in this patterns we never change the original values
-// but mutate copies of values plus the changes
-
-// const copied_array = hobbies.slice()
-// const copied_array = [...hobbies] // Array copied with spread operator
-// console.log(copied_array)
-
-// The REST operator
-// const toArray = (arg1, arg2, arg3) => {
-//   return [arg1, arg2, arg3]
-// }
-
-// const toArray = (...args) => {
-//   return args
-// }
-// console.log(toArray(1, 2, 3))
-// // If we call the same function with fourth argument, nothing changes. We still see output with 3 args
-// console.log(toArray(1, 2, 3, 4))
+console.log('Hello')
+console.log('Hi!')
