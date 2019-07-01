a0b0362edd1fa50b7846c314c73fb8b620fdc691
Robert Gersak <robi@neyho.com>
2019-07-01 08:53:00 +0200

    Sending responses

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 const http = require('http')
 
-// const rqListener = (req, res) => {
-//
-// }
-// // rqListener is passed as reference to createServer
-// // it tells the createServer function to call rqListener for every incoming
-// // request
-// http.createServer(rqListener)
-
-// The other way to create server is to pass an anonymous function to createServer
-// arguments
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers)
-  // process.exit()
 
+  res.setHeader('Content-Type', 'text/html')
+  res.write(`
+    <html>
+      <head>
+        <title>Web Page</title>
+      </head>
+      <body>
+        <h1>Hello from my Node.js Server</h1>
+      </body>
+    </html>
+    `)
+  res.end()
 })
 
 server.listen(3000)
