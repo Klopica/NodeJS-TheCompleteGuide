a81b8fb3a0247de52ae5ac1a3aa56838f665a4f7
Robert Gersak <robi@neyho.com>
2019-07-01 13:02:44 +0200

    Installing Express.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const http = require('http')
-const routes = require('./routes')
 
-// Using the Node Modules System
-// Move server routing logic to a new file "routes.js"
+const express = require('express')
 
-// const server = http.createServer(routes)
-const server = http.createServer(routes.handler)
-console.log(routes.someText)
+const app = express()
+
+const server = http.createServer(app)
 
 server.listen(3000)
