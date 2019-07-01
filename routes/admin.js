64244b6cf668da2ac3e9838c71073fb09e3aca4f
Robert Gersak <robi@neyho.com>
2019-07-01 15:46:51 +0200

    Using a helper function for Navigation

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,8 @@ const path = require('path')
 const express = require('express')
 const router = express.Router()
 
+const rootDir = require('../util/path')
+
 router.get('/add-product', (req, res, next) => {
   // console.log('In another Middleware - Products page!')
   // res.send(`
@@ -12,7 +14,7 @@ router.get('/add-product', (req, res, next) => {
   //     <button type="submit">Submit</button>
   //   </form>
   // `)
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 router.post('/add-product', (req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -3,12 +3,13 @@ const path = require('path')
 // an absolute path fou you
 
 const express = require('express')
-
 const router = express.Router()
+
+const rootDir = require('../util/path')
 
 router.get('/', (req, res, next) => {
   // res.send('<h1>Hello from Express!</h1>')
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
 
 module.exports = router
