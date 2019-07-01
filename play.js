f24d2fff15c44269a3b9fbff38fa3417f0b3a7e1
Robert Gersak <robi@neyho.com>
2019-07-01 06:49:49 +0200

    let, const, arrow functions

--- play.js
+++ play.js
@@ -16,4 +16,14 @@ const summarizeUser = (userName, userAge, userHasHobby) => {
   ' and the user has hobbies ' + userHasHobby
 }
 
+// const add = (a, b) => {
+//   return a + b
+// }
+
+const add = (a, b) => a + b
+const addOne = a => a + 1
+const addRandom = () => 1 + 2
+
+console.log(addRandom(1))
+
 console.log(summarizeUser(name, age, hasHobbies))
