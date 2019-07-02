808b81c4ed0ba1df4ab66f622d193ce394e69565
Klopica <klopjr@gmail.com>
2019-07-02 14:58:15 +0200

    Storing data in files Via the Model

--- models/product.js
+++ models/product.js
@@ -1,11 +1,24 @@
-const products = []
+const fs = require('fs')
+const path = require('path')
+
 module.exports = class Product {
   constructor (title) {
     this.title = title
   }
 
   save () {
-    products.push(this)
+    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+    // First read this file to get existing data
+    fs.readFile(p, (err, data) => {
+      let products = []
+      if(!err) {
+        products = JSON.parse(data)
+      }
+      products.push(this)
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err)
+      })
+    })
   }
 
   // STATIC keyword makes sure that this method can be called directly on the
@@ -13,6 +26,12 @@ module.exports = class Product {
   // This means we can call this function without keyword "new"
   // example is in /controllers/products.js
   static fetchAll () {
-    return products
+    const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json')
+    fs.readFile(p, (err, data) => {
+      if(err) {
+        return []
+      }
+      return JSON.parse(data)
+    })
   }
 }
