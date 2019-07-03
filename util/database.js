b5816d12067a48128410e15b3a16767a005c7e14
Klopica <klopjr@gmail.com>
2019-07-03 08:57:58 +0200

    Finished SQL code

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
+const sequelize = new Sequelize('node-complete', 'root', 'Klopica#1303', {
   dialect: 'mysql',
   host: 'localhost'
 });
